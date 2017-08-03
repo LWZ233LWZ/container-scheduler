b5f9e14586f6cb95f66f97ff34ec1d54571a5e5f
xu fei <knightXun@users.noreply.github.com>
2017-08-03 16:21:08 +0800

    <feature>: types and its marshal/unmarshal

--- types/types.py
+++ types/types.py
@@ -1,4 +1,5 @@
 import time
+import schema
 
 NamespaceDefault = "default"
 NamespaceAll = ''
@@ -9,12 +10,36 @@ NamespacePublic = "kube-public"
 EventTypeNormal = 'Normal'
 EventTypeWarning = 'Warning'
 
+def marshal(func):
+    pass
+
+def unmarshal(func):
+    pass
+
+@unmarshal
+@marshal
 class TypeMeta(object):
+    jsonMap = {
+        'Kind' : 'json:"kind,omitempty" protobuf:"bytes,1,opt,name=kind"',
+        'APIVersion': 'json:"apiVersion,omitempty" protobuf:"bytes,2,opt,name=apiVersion"',
+    }
     def __init__(self):
         self.Kind = ''
         self.APIVersion = ''
 
+@marshal
+@unmarshal
 class OwnerReference(object):
+    jsonMap = {
+        'APIVersion': 'json:"apiVersion" protobuf:"bytes,5,opt,name=apiVersion"',
+        'Kind' : 'json:"kind" protobuf:"bytes,1,opt,name=kind"',
+        'Name': 'json:"name" protobuf:"bytes,3,opt,name=name"',
+        'UID': 'json:"uid" protobuf:"bytes,4,opt,name=uid"',
+        'Controller':'json:"controller,omitempty" protobuf:"varint,6,opt,name=controller',
+        'BlockOwnerDeletion': 'json:"blockOwnerDeletion,omitempty" protobuf:"varint,7,opt,name=blockOwnerDeletion"',
+    }
+    def __call__(self, *args, **kwargs):
+        pass
     def __init__(self):
         self.APIVersion = None
         self.Kind = None
@@ -23,7 +48,27 @@ class OwnerReference(object):
         self.Controller = None
         self.BlockOwnerDeletion = None
 
+@marshal
+@unmarshal
 class ObjectMeta(object):
+    jsonMap = {
+        'Name': 'json:"name,omitempty" protobuf:"bytes,1,opt,name=name"',
+        'GenerateName': 'json:"generateName,omitempty" protobuf:"bytes,2,opt,name=generateName"',
+        'Namespace': 'json:"namespace,omitempty" protobuf:"bytes,3,opt,name=namespace"',
+        'SelfLink' : 'json:"selfLink,omitempty" protobuf:"bytes,4,opt,name=selfLink"',
+        'UID':      'json:"uid,omitempty" protobuf:"bytes,5,opt,name=uid,casttype=k8s.io/kubernetes/pkg/types.UID"',
+        'ResourceVersion': 'json:"resourceVersion,omitempty" protobuf:"bytes,6,opt,name=resourceVersion"',
+        'Generation': 'json:"generation,omitempty" protobuf:"varint,7,opt,name=generation"',
+        'CreationTimestamp': 'json:"creationTimestamp,omitempty" protobuf:"bytes,8,opt,name=creationTimestamp"',
+        'DeletionTimestamp':  'json:"deletionTimestamp,omitempty" protobuf:"bytes,9,opt,name=deletionTimestamp"',
+        'DeletionGracePeriodSeconds': 'json:"deletionGracePeriodSeconds,omitempty" protobuf:"varint,10,opt,name=deletionGracePeriodSeconds"',
+        'Labels': 'json:"labels,omitempty" protobuf:"bytes,11,rep,name=labels"',
+        'Annotations': 'json:"annotations,omitempty" protobuf:"bytes,12,rep,name=annotations"',
+        'OwnerReferences': 'json:"ownerReferences,omitempty" patchStrategy:"merge" patchMergeKey:"uid" protobuf:"bytes,13,rep,name=ownerReferences"',
+        'Finalizers': 'json:"finalizers,omitempty" patchStrategy:"merge" protobuf:"bytes,14,rep,name=finalizers"',
+        'ClusterName': 'json:"clusterName,omitempty" protobuf:"bytes,15,opt,name=clusterName"'
+    }
+
     def __init__(self):
         self.Name = ''
         self.GenerateName = ''
@@ -42,6 +87,15 @@ class ObjectMeta(object):
         self.ClusterName = ''
 
 class ObjectReference(object):
+    jsonMap = {
+        'Kind': 'json:"kind,omitempty" protobuf:"bytes,1,opt,name=kind"',
+        'Namespace': 'json:"namespace,omitempty" protobuf:"bytes,2,opt,name=namespace"',
+        'Name': 'json:"name,omitempty" protobuf:"bytes,3,opt,name=name"',
+        'UID': 'json:"uid,omitempty" protobuf:"bytes,4,opt,name=uid,casttype=k8s.io/apimachinery/pkg/types.UID"',
+        'APIVersion': 'json:"apiVersion,omitempty" protobuf:"bytes,5,opt,name=apiVersion"',
+        'ResourceVersion': 'json:"resourceVersion,omitempty" protobuf:"bytes,6,opt,name=resourceVersion"',
+        'FieldPath': 'json:"fieldPath,omitempty" protobuf:"bytes,7,opt,name=fieldPath"',
+    }
     def __init__(self):
         self.Kind = ''
         self.Namespace = ''
@@ -52,12 +106,33 @@ class ObjectReference(object):
         self.FieldPath = ''
 
 class EventSource(object):
+    jsonMap = {
+       'Component': 'json:"component,omitempty" protobuf:"bytes,1,opt,name=component"',
+        'Host': 'json:"host,omitempty" protobuf:"bytes,2,opt,name=host"',
+    }
+
     def __init__(self):
         self.Component = ''
         self.Host = ''
 
 class Event(TypeMeta,ObjectMeta):
+    jsonMap = {
+        'TypeMeta': 'json:",inline"',
+        'ObjectMeta': 'json:"metadata" protobuf:"bytes,1,opt,name=metadata"',
+        'InvolvedObject': 'json:"involvedObject" protobuf:"bytes,2,opt,name=involvedObject"',
+        'Reason': 'json:"reason,omitempty" protobuf:"bytes,3,opt,name=reason"',
+        'Message': 'json:"message,omitempty" protobuf:"bytes,4,opt,name=message"',
+        'Source': 'json:"source,omitempty" protobuf:"bytes,5,opt,name=source"',
+        'FirstTimestamp': '`json:"firstTimestamp,omitempty" protobuf:"bytes,6,opt,name=firstTimestamp"`',
+        'LastTimestamp': 'json:"lastTimestamp,omitempty" protobuf:"bytes,7,opt,name=lastTimestamp"',
+        'Count': 'json:"count,omitempty" protobuf:"varint,8,opt,name=count"',
+        'Type': 'json:"type,omitempty" protobuf:"bytes,9,opt,name=type"',
+    }
+
     def __init__(self):
+        super.__init__(self,TypeMeta).__init__()
+        super.__init__(self.ObjectMeta).__init__()
+
         self.InvolvedObject = ObjectReference()
         self.Reason = ''
         self.Message = ''
@@ -87,18 +162,13 @@ class LeaderElectionRecord(object):
 class LockInterface(object):
     def Get(self): pass
     def Create(self): pass
-    
+
 class LeaderElectionConfiguration(object):
     def __init__(self):
         self.LeaderElect = bool()
         self.LeaseDuration = Duration()
         self.RenewDeadline = Duration()
         self.RetryPeriod = Duration()
-
-class TypeMeta(object):
-    def __init__(self):
-        self.Kind = ''
-        self.APIVersion = ''
 
 class KubeSchedulerConfiguration(TypeMeta):
     def __init__(self):
@@ -115,5 +185,9 @@ class KubeSchedulerConfiguration(TypeMeta):
         self.SchedulerName = ''
         self.HardPodAffinitySymmetricWeight = int()
         self.FailureDomains = ''
+
+if __name__ == '__main__':
+    e = Event()
+    print e.jsonMap
 
 
